878f5b0f5c854f327879c02a8facc6af34bf3a4e
P Patricia <ppatricia.syah@gmail.com>
2023-06-16 14:13:41 +0700

    Update models.py

--- web/models.py
+++ web/models.py
@@ -15,9 +15,9 @@ class Diary(db.Model):
     rekomendasi = db.Column(db.String(10000))
 
 
-#class User(db.Model, UserMixin):
- #   id = db.Column(db.Integer, primary_key=True)
-  #  email = db.Column(db.String(150), unique=True)
-   # password = db.Column(db.String(150))
-    #first_name = db.Column(db.String(150))
-   # diary = db.relationship('Diary')
+# class User(db.Model, UserMixin):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(150), unique=True)
+#     password = db.Column(db.String(150))
+#     first_name = db.Column(db.String(150))
+#     diary = db.relationship('Diary')
